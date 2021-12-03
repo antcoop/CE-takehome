2dbb280c129dc1318a13b6bb5e6ae1966c0ac0d1
antcoop <acooper@hireology.com>
2021-12-03 11:28:31 -0600

    added comments of decisions made for refactor

--- Bug/index.js
+++ Bug/index.js
@@ -1,5 +1,11 @@
+// Housekeeping
 const userInput = process.argv[2];
 
+/** 
+ * Moved practiceCoding into global scope and allowed it to accept parameters.
+ * This increases purity for reuse in necessary.
+ * @param {string} input - type of input to check against to resolve or reject
+*/
 const practiceCoding = (input) => new Promise((resolve, reject) => {
   if (input) console.log('Current user activity:', input);
   
@@ -17,6 +23,11 @@ const practiceCoding = (input) => new Promise((resolve, reject) => {
   }
 });
 
+/**
+ * Created an intializer so it's easier to identify where the program starts.
+ * Also, added an input argument to increase purity
+ * @param {string} input - type of input to check against to resolve or reject
+ */
 const init = async (input) => {
   try {
     const result = await practiceCoding(input);
@@ -27,4 +38,5 @@ const init = async (input) => {
   process.exit();
 };
 
+/** Intializer accepts the user input from process environment variables */
 init(userInput);
